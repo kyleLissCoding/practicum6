0f5af1846298d84451f6d998ef9cbb13c4664e34
kyleliss <kliss@iu.edu>
2025-04-02 17:10:48 -0400

    adding new azure properties as well as edits for pract 8

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#
+# Deploys the microservice to Kubernetes.
+#
+# Assumes the image has already been built and published to the container registry.
+#
+# Environment variables:
+#
+#
+# CONTAINER_REGISTRY The hostname of your container registry.
+# VERSION The version number of the image to deploy.
+#
+# Usage:
+#
+# ./deploy.sh
+#
+
+set -u
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+envsubst < ./deploy.yaml | kubectl apply -f -
